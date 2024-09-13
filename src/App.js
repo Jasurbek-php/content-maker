f60702098243bcdabd100d622c9dbf82703393a0
Jasurbek <jasurbeksadriddinov112@gmail.com>
2024-09-14 00:59:39 +0500

    Zo'r

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Card from './component/Card/Card';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <React.StrictMode>
+
+        <Card />
+      </React.StrictMode>
+
+    </div>
+  );
+}
+
+export default App;
